ff48a7bc0b0dbb202944854b311be35b4205477d
Lam Vinh <builamvinh2000@gmail.com>
2021-03-29 09:54:53 +0700

    2 thang cho ngu clone ve sua

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from cv2 import cv2
 from keras.models import load_model
 import numpy as np
 from keras.applications import ResNet152
-import pickle
+from tts import text_to_speech
 from fearture import feature_cap
+from flask_cors import cross_origin
 resnet = ResNet152(include_top=False, weights='imagenet',
                    input_shape=(224, 224, 3), pooling='avg')
 # resnet = load_model('resnet.h5')
-
 app = Flask(__name__)
 
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
@@ -19,16 +19,16 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/after', methods=['GET', 'POST'])
+@app.route('/after', methods=['POST', 'GET'])
+@cross_origin()
 def after():
     global resnet
-    rq = request.files['file1']
-    rq.save('static/test1.mp4')
-    rq = str(rq).split(' ')
-    rq = rq[1].replace("'", '')
-    url_video = 'static/' + rq
-
-    fe = feature_cap(resnet, url_video)
+    rq = request.files['file']
+    rq.save('static/test.mp4')
+    fe = feature_cap(resnet, 'static/test.mp4')
+    # data = 'cho trieu'
+    gender = request.form['voices']
+    text_to_speech('hi my name trieu  stupid', gender)
     return render_template('after.html', data=fe)
 
 
---
+++ load_trg.py
@@ -0,0 +1,29 @@
+import re
+import pickle
+from torchtext import data
+import spacy
+import pathlib
+
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+
+class tokenize(object):
+
+    def __init__(self, lang):
+        self.nlp = spacy.load(lang)
+
+    def tokenizer(self, sentence):
+        sentence = re.sub(
+            r"[\*\"“”\n\\…\+\-\/\=\(\)‘•:\[\]\|’\!;]", " ", str(sentence))
+        sentence = re.sub(r"[ ]+", " ", sentence)
+        sentence = re.sub(r"\!+", "!", sentence)
+        sentence = re.sub(r"\,+", ",", sentence)
+        sentence = re.sub(r"\?+", "?", sentence)
+        sentence = sentence.lower()
+        return [tok.text for tok in self.nlp.tokenizer(sentence) if tok.text != " "]
+
+
+with (open('data\cab.pkl', 'rb')) as pickel_file:
+    TRG = pickle.load(pickel_file)
+print(len(TRG.vocab))
