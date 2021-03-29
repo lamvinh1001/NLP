3c5a38bc1c51731d11482bd8142450a457d0029d
Lam Vinh <builamvinh2000@gmail.com>
2021-03-29 12:54:47 +0700

    debug model.py

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ from torchtext import data
 from torch.autograd import Variable
 import torch.nn as nn
 import torch
-
+import copy
 pathlib.PosixPath = pathlib.WindowsPath
 
 
@@ -166,13 +166,343 @@ def translate_sentence(fea, model, TRG, device, k, max_len):
     return multiple_replace({' ?': '?', ' !': '!', ' .': '.', '\' ': '\'', ' ,': ','}, sentence)
 
 
+class PositionalEncoder(nn.Module):
+    """
+    Lớp positional dùng để thêm vị trí của các từ trong câu.
+    Nằm sau lớp emdeder
+    """
+
+    def __init__(self, d_model, max_seq_length=200, dropout=0.1):
+        super().__init__()
+
+        self.d_model = d_model
+        self.dropout = nn.Dropout(dropout)
+
+        pe = torch.zeros(max_seq_length, d_model)
+
+        for pos in range(max_seq_length):
+            for i in range(0, d_model, 2):
+                pe[pos, i] = math.sin(pos/(10000**(2*i/d_model)))
+                pe[pos, i+1] = math.cos(pos/(10000**((2*i+1)/d_model)))
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+
+        x = x*math.sqrt(self.d_model)
+        seq_length = x.size(1)
+
+        pe = Variable(self.pe[:, :seq_length], requires_grad=False)
+
+        if x.is_cuda:
+            pe.cuda()
+        # cộng embedding vector với pe
+        x = x + pe
+        x = self.dropout(x)
+
+        return x
+
+
+class Norm(nn.Module):
+    """
+    lớp norm lize chuẩn hoá dữ liệu
+    """
+
+    def __init__(self, d_model, eps=1e-6):
+        super().__init__()
+
+        self.size = d_model
+
+        # create two learnable parameters to calibrate normalisation
+        self.alpha = nn.Parameter(torch.ones(self.size))
+        self.bias = nn.Parameter(torch.zeros(self.size))
+
+        self.eps = eps
+
+    def forward(self, x):
+        norm = self.alpha * (x - x.mean(dim=-1, keepdim=True)) \
+            / (x.std(dim=-1, keepdim=True) + self.eps) + self.bias
+        return norm
+
+# tinh multiHeadAttention
+# Cơ chế attention trong model
+
+
+def attention(q, k, v, mask=None, dropout=None):
+    """
+    1 lớp attention dùng để học các đặc tính có liên quan với nhau nằm ở sau
+    lớp positional
+    q: batch_size x head x seq_length x d_model
+    k: batch_size x head x seq_length x d_model
+    v: batch_size x head x seq_length x d_model
+    mask: batch_size x 1 x 1 x seq_length
+    output: batch_size x head x seq_length x d_model
+    """
+
+    # attention score được tính bằng cách nhân q với k
+    d_k = q.size(-1)
+    scores = torch.matmul(q, k.transpose(-2, -1))/math.sqrt(d_k)
+
+    if mask is not None:
+        mask = mask.unsqueeze(1)
+        scores = scores.masked_fill(mask == 0, -1e9)
+    # chuẩn hóa bằng softmax
+    scores = F.softmax(scores, dim=-1)
+
+    if dropout is not None:
+        scores = dropout(scores)
+
+    output = torch.matmul(scores, v)
+    return output, scores
+
+
+class MultiHeadAttention(nn.Module):
+    """
+    Mô hình có nhiều lớp ettention
+    """
+
+    def __init__(self, heads, d_model, dropout=0.1):
+        super().__init__()
+        assert d_model % heads == 0
+
+        self.d_model = d_model
+        self.d_k = d_model//heads
+        self.h = heads
+        self.attn = None
+
+        # tạo ra 3 ma trận trọng số là q_linear, k_linear, v_linear
+        self.q_linear = nn.Linear(d_model, d_model)
+        self.k_linear = nn.Linear(d_model, d_model)
+        self.v_linear = nn.Linear(d_model, d_model)
+
+        self.dropout = nn.Dropout(dropout)
+        self.out = nn.Linear(d_model, d_model)
+
+    def forward(self, q, k, v, mask=None):
+        """
+        q: batch_size x seq_length x d_model
+        k: batch_size x seq_length x d_model
+        v: batch_size x seq_length x d_model
+        mask: batch_size x 1 x seq_length
+        output: batch_size x seq_length x d_model
+        """
+        bs = q.size(0)
+        # nhân ma trận trọng số q_linear, k_linear, v_linear với dữ liệu đầu vào q, k, v
+        # ở bước encode các bạn lưu ý rằng q, k, v chỉ là một
+        q = self.q_linear(q).view(bs, -1, self.h, self.d_k)
+        k = self.k_linear(k).view(bs, -1, self.h, self.d_k)
+        v = self.v_linear(v).view(bs, -1, self.h, self.d_k)
+
+        q = q.transpose(1, 2)
+        k = k.transpose(1, 2)
+        v = v.transpose(1, 2)
+
+        # tính attention score
+        scores, self.attn = attention(q, k, v, mask, self.dropout)
+
+        concat = scores.transpose(1, 2).contiguous().view(bs, -1, self.d_model)
+
+        output = self.out(concat)
+        return output
+
+
+class FeedForward(nn.Module):
+    """ Trong kiến trúc của chúng ta có tầng linear
+    """
+
+    def __init__(self, d_model, d_ff=2048, dropout=0.1):
+        super().__init__()
+
+        # We set d_ff as a default to 2048
+        self.linear_1 = nn.Linear(d_model, d_ff)
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff, d_model)
+
+    def forward(self, x):
+        x = self.dropout(F.relu(self.linear_1(x)))
+        x = self.linear_2(x)
+        return x
+
+
+class EncoderLayer(nn.Module):
+    """
+    1 khối encoder gồm input và nhiều lớp attention giúp học được các đặc tính
+    """
+
+    def __init__(self, d_model, heads, dropout=0.1):
+        super().__init__()
+        self.norm_1 = Norm(d_model)
+        self.norm_2 = Norm(d_model)
+        self.attn = MultiHeadAttention(heads, d_model, dropout=dropout)
+        self.ff = FeedForward(d_model, dropout=dropout)
+        self.dropout_1 = nn.Dropout(dropout)
+        self.dropout_2 = nn.Dropout(dropout)
+
+    def forward(self, x, mask):
+        """
+        x: batch_size x seq_length x d_model
+        mask: batch_size x 1 x seq_length
+        output: batch_size x seq_length x d_model
+        """
+
+        x2 = self.norm_1(x)
+        # tính attention value, các bạn để ý q, k, v là giống nhau
+        x = x + self.dropout_1(self.attn(x2, x2, x2, mask))
+        x2 = self.norm_2(x)
+        x = x + self.dropout_2(self.ff(x2))
+        return x
+
+
+def get_clones(module, N):
+    return nn.ModuleList([copy.deepcopy(module) for i in range(N)])
+
+
+class Encoder(nn.Module):
+    """Một encoder có nhiều encoder layer với input là các feature của video
+    """
+
+    def __init__(self, d_model, N, heads, dropout):
+        super().__init__()
+        self.N = N
+        self.pe = PositionalEncoder(d_model, dropout=dropout)
+        self.layers = get_clones(EncoderLayer(d_model, heads, dropout), N)
+        self.norm = Norm(d_model)
+
+    def forward(self, src, mask):
+        """
+        src: batch_size x seq_length
+        mask: batch_size x 1 x seq_length
+        output: batch_size x seq_length x d_model
+        """
+        # x = self.pe(src)
+        x = src
+        for i in range(self.N):
+            x = self.layers[i](x, mask)
+        return self.norm(x)
+
+# decoder mo hinhh transformer
+
+# 1 khoi decoder
+
+
+class Embedder(nn.Module):
+    """
+    tầng embedder dùng để vector hoá các câu đầu vào input của tầng decoder
+    """
+
+    def __init__(self, vocab_size, d_model):
+        super().__init__()
+        self.vocab_size = vocab_size
+        self.d_model = d_model
+
+        self.embed = nn.Embedding(vocab_size, d_model)
+
+    def forward(self, x):
+        return self.embed(x)
+
+
+class DecoderLayer(nn.Module):
+    """
+    1 khối decoder gồm output và các mutiattention giúp học các đặc tính của output
+    """
+
+    def __init__(self, d_model, heads, dropout=0.1):
+        super().__init__()
+        self.norm_1 = Norm(d_model)
+        self.norm_2 = Norm(d_model)
+        self.norm_3 = Norm(d_model)
+
+        self.dropout_1 = nn.Dropout(dropout)
+        self.dropout_2 = nn.Dropout(dropout)
+        self.dropout_3 = nn.Dropout(dropout)
+
+        self.attn_1 = MultiHeadAttention(heads, d_model, dropout=dropout)
+        self.attn_2 = MultiHeadAttention(heads, d_model, dropout=dropout)
+        self.ff = FeedForward(d_model, dropout=dropout)
+
+    def forward(self, x, e_outputs, src_mask, trg_mask):
+        """
+        x: batch_size x seq_length x d_model
+        e_outputs: batch_size x seq_length x d_model
+        src_mask: batch_size x 1 x seq_length
+        trg_mask: batch_size x 1 x seq_length
+        """
+        x2 = self.norm_1(x)
+        # multihead attention thứ nhất, chú ý các từ ở target
+        x = x + self.dropout_1(self.attn_1(x2, x2, x2, trg_mask))
+        x2 = self.norm_2(x)
+
+        # masked mulithead attention thứ 2. k, v là giá trị output của mô hình encoder
+        x = x + self.dropout_2(self.attn_2(x2, e_outputs, e_outputs, src_mask))
+        x2 = self.norm_3(x)
+        x = x + self.dropout_3(self.ff(x2))
+        return x
+
+
+class Decoder(nn.Module):
+    """Một decoder có nhiều decoder layer với input là các câu caption qua 1 tầng decoder
+    rồi concat với output của encoder làm input của tấng decoder tiếp theo
+    """
+
+    def __init__(self, vocab_size, d_model, N, heads, dropout):
+        super().__init__()
+        self.N = N
+        self.embed = Embedder(vocab_size, d_model)
+        self.pe = PositionalEncoder(d_model, dropout=dropout)
+        self.layers = get_clones(DecoderLayer(d_model, heads, dropout), N)
+        self.norm = Norm(d_model)
+
+    def forward(self, trg, e_outputs, src_mask, trg_mask):
+        """
+        trg: batch_size x seq_length
+        e_outputs: batch_size x seq_length x d_model
+        src_mask: batch_size x 1 x seq_length
+        trg_mask: batch_size x 1 x seq_length
+        output: batch_size x seq_length x d_model
+        """
+        x = self.embed(trg)
+        x = self.pe(x)
+        for i in range(self.N):
+            x = self.layers[i](x, e_outputs, src_mask, trg_mask)
+        return self.norm(x)
+
+
+class Transformer(nn.Module):
+    """ Cuối cùng ghép chúng lại với nhau để được mô hình transformer hoàn chỉnh
+    gồm 2 lớp encoder và decoder
+    """
+
+    def __init__(self, trg_vocab, d_model, N, heads, dropout):
+        super().__init__()
+        self.linear = nn.Linear(2048, d_model)
+        self.encoder = Encoder(d_model, N, heads, dropout)
+        self.decoder = Decoder(trg_vocab, d_model, N, heads, dropout)
+        self.out = nn.Linear(d_model, trg_vocab)
+
+    def forward(self, src, trg, src_mask, trg_mask):
+        """
+        src: batch_size x seq_length
+        trg: batch_size x seq_length
+        src_mask: batch_size x 1 x seq_length
+        trg_mask batch_size x 1 x seq_length
+        output: batch_size x seq_length x vocab_size
+        """
+
+        ln = self.linear(src)
+        e_outputs = self.encoder(ln, src_mask)
+
+        d_output = self.decoder(trg, e_outputs, src_mask, trg_mask)
+        output = self.out(d_output)
+        return output
+
+
 if __name__ == '__main__':
 
     opt = {
         'trg_lang': 'en',
         'max_strlen': 160,
         'batchsize': 1500,
-        'device': 'cuda',
+        'device': 'cpu',
         'd_model': 512,
         'n_layers': 6,
         'heads': 8,
@@ -182,11 +512,14 @@ if __name__ == '__main__':
         'printevery': 100,
         'k': 5,
     }
-    print(torch.__version__)
-    with open('cab.pkl', 'rb') as pickle_file:
+    with open('data\cab.pkl', 'rb') as pickle_file:
         TRG = pickle.load(pickle_file)
-    model = torch.load('model_test.pt', map_location=torch.device('cpu'))
+    model_tran = Transformer(
+        len(TRG.vocab), opt['d_model'], opt['n_layers'], opt['heads'], opt['dropout'])
+
+    model_tran.load_state_dict(torch.load(
+        'model_test.pt', map_location=torch.device('cpu'))['state_dict'])
     fear = np.load('testv1RN40.npy')
     trans_sent = translate_sentence(
-        fear, model, TRG, opt['device'], opt['k'], opt['max_strlen'])
+        fear, model_tran, TRG, opt['device'], opt['k'], opt['max_strlen'])
     print('predict: ', trans_sent)
